9ceb9e44dceda54a0ff61e84382da154947e9e7d
viswanath27 <code.vissu@gmail.com>
2023-06-19 09:12:06 +0530

    updated the code with lot of stuffs

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, jsonify
+#Create Embeddings of your documents to get ready for semantic search 
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings 
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import pinecone 
+import os
 
 app = Flask(__name__)
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', '0b0244fc-fee8-43c0-8f6a-ec6bad3b2dcc')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'northamerica-northeast1-gcp') 
+docsearch = None
+
 
 @app.route("/")
 def index():
@@ -23,17 +39,65 @@ def create_file():
 
 @app.route("/chatbot", methods=["POST"])
 def chatbot_endpoint():
+    global docsearch
     user_message = request.json.get("message")
     if user_message:
         # Process the user message and generate a bot response
         # Replace the code below with your chatbot logic
-        bot_message = f"Bot: You said '{user_message}'"
-        return {"message": bot_message}
+        # bot_message = f"Bot: You said '{user_message}'"
+        # query = "What are examples of good data science teams ?"
+            #Load your data
+        loader = UnstructuredPDFLoader(os.path.join(os.getcwd(), 'uploads', "field-guide-to-data-science.pdf"))
+        print("Done with loading the pdf file...")
+
+        data = loader.load()
+
+        print(f'You have {len(data)} documents in your data')
+        print(f'There are  {len(data[0].page_content)} characters in your documents')
+
+        # Chunk your data up into smaller documents 
+        text_spliter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=0)
+        texts = text_spliter.split_documents(data)
+
+        print(f'Now you have {len(texts)} documents')
+
+
+        OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+        PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', '0b0244fc-fee8-43c0-8f6a-ec6bad3b2dcc')
+        PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'northamerica-northeast1-gcp')
+
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+        pinecone.init(
+            api_key = PINECONE_API_KEY,
+            environment=PINECONE_API_ENV
+        )
+        index_name = "datascience-book"
+
+        docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+
+        query = user_message
+        print(f"Query:{query}")
+        docs = docsearch.similarity_search(query)
+
+        # Here's an example of the first document that was returned 
+        # print(docs[0].page_content[:450])
+
+        #Query those docs to get your answer back 
+        llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+        chain = load_qa_chain(llm, chain_type="stuff")
+
+        query= "What is the Impact of Data Science?"
+        docs = docsearch.similarity_search(query)
+        answer = chain.run(input_documents=docs, question=query)
+        print(f"Answer:{answer}")
+        return {"message": answer}
     return {"error": "No message provided."}
 
 
 @app.route("/train-function", methods=["POST"])
 def my_function():
+    global docsearch
     # Implement your function logic here
     # Access any data passed from JavaScript using request.json
     # Perform any necessary computations or actions
@@ -41,11 +105,42 @@ def my_function():
 
     # For example, you can return a JSON response
     print("This is button click")
+    #Load your data
+    loader = UnstructuredPDFLoader(os.path.join(os.getcwd(), 'uploads', "field-guide-to-data-science.pdf"))
+    print("Done with loading the pdf file...")
+
+    data = loader.load()
+
+    print(f'You have {len(data)} documents in your data')
+    print(f'There are  {len(data[0].page_content)} characters in your documents')
+
+    # Chunk your data up into smaller documents 
+    text_spliter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=0)
+    texts = text_spliter.split_documents(data)
+
+    print(f'Now you have {len(texts)} documents')
+
+
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+    PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', '0b0244fc-fee8-43c0-8f6a-ec6bad3b2dcc')
+    PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'northamerica-northeast1-gcp')
+
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    pinecone.init(
+        api_key = PINECONE_API_KEY,
+        environment=PINECONE_API_ENV
+    )
+    index_name = "datascience-book"
+
+    docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
 
     response = {"message": "Function called successfully"}
     return jsonify(response)
 
-
+@app.route("/voice")
+def voice():
+    return render_template("voice.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
